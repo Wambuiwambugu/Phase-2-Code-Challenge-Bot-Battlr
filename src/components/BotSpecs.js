7f8b4c4800d6e61b6444437ffd2726c4582d819e
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-05-30 18:53:40 +0300

    Display BotSpecs

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -9,9 +9,11 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotSpecs({ bot }) {
+function BotSpecs({ bot,unmount,handleAdd}) {
   return (
+    
     <div className="ui segment">
+      <p>bot specs</p>
       <div className="ui two column centered grid">
         <div className="row">
           <div className="four wide column">
@@ -52,19 +54,13 @@ function BotSpecs({ bot }) {
             </div>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log("connect this to a function that shows all bots")
-              }
+              onClick={unmount }
             >
               Go Back
             </button>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
-              }
+              onClick={handleAdd}
             >
               Enlist
             </button>
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+import BotSpecs from "./BotSpecs";
 
 function BotsPage() {
   //start here with your code for step one
   const [bots, setBots] = useState([]);
   const [armybots, setArmyBots] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+  const [botToDisplay, setBotToDisplay] = useState({})
 
   useEffect(() => {
     fetch("http://localhost:8002/bots")
     .then(res => res.json())
     .then(data => setBots(data))
   },[]);
-  function manageBotArmy(chosenBot){
-    if (!armybots.includes(chosenBot)){
-      setArmyBots([...armybots,chosenBot])  
+  
+  
+  function displayBots(chosenBot){
+    if (chosenBot){
+       setIsClicked(true);
+       setBotToDisplay(chosenBot)
     };
+  };
+  function specsUnmount(){
+    setIsClicked(false)
   }
+  function addToArmy(){
+    if (!armybots.includes(botToDisplay)){
+      setArmyBots([...armybots,botToDisplay]);
+    }
+  }
+
   function handleRemove(removedBot){
     if (armybots.includes(removedBot)){
       setArmyBots(armybots.filter((bot) => bot.id !== removedBot.id))
@@ -31,17 +46,26 @@ function BotsPage() {
     .then(res => res.json())
     .then(data => console.log(data))
 
-    setBots(bots.filter((bot) => bot.id !== deletedBot.id))
-    
-
+    setBots(bots.filter((bot) => bot.id !== deletedBot.id));
+    setArmyBots(armybots.filter((bot) => bot.id !== deletedBot.id));
+  };
+  
+  
+  const botCollection = () => {
+    return <BotCollection botData={bots} chooseBots={displayBots} deleteBot={handleDeleteBot} />
+  };
+  const botSpecs = () => {
+    return <BotSpecs bot={botToDisplay} unmount={specsUnmount} handleAdd={addToArmy}/>
   }
+  const display = isClicked? botSpecs : botCollection
+  
 
     
     
   return (
     <div>
       <YourBotArmy armyBotData={armybots} removedBots={handleRemove} deleteBot={handleDeleteBot} />
-      <BotCollection botData={bots} chooseBots={manageBotArmy} deleteBot={handleDeleteBot} />
+     {display()}
     </div>
   )
 }
